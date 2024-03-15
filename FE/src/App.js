e4d848ba2dd559963ad871c8bc4cb69be5c5b043
m-ensr <ensar.ozdemir@yahoo.com>
2024-03-15 18:14:56 +0300

    Update App.js

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,35 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home';
-import Journey from './pages/journey/Journey';
-import Serendipity from './pages/serendipity/Serendipity';
-import About from './pages/about/About';
-import background from './background.mp4';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Journey from "./pages/journey/Journey";
+import Journeying from "./pages/journey/Journeying";
+import Serendipity from "./pages/serendipity/Serendipity";
+import About from "./pages/about/About";
+import background from "./background.mp4";
 
 const App = () => {
   return (
     <Router>
-      <div style={{ position: 'relative' }}>
+      <div style={{ position: "relative" }}>
         <video
           autoPlay
           muted
           loop
           style={{
-            position: 'fixed',
+            position: "fixed",
             top: 0,
             left: 0,
             zIndex: -1,
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
           }}
         >
           <source src={background} type="video/mp4" />
         </video>
-        <div style={{ position: 'relative', zIndex: 1 }}>
+        <div style={{ position: "relative", zIndex: 1 }}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/journey" element={<Journey />} />
+            <Route path="/journeying" element={<Journeying />} />
             <Route path="/serendipity" element={<Serendipity />} />
             <Route path="/about" element={<About />} />
           </Routes>
