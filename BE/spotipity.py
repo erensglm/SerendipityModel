3328c5e12a1907e9bc755f2b91489a2e720c312f
m-ensr <ensar.ozdemir@yahoo.com>
2024-03-24 14:59:14 +0300

    Update spotipity.py

--- BE/spotipity.py
+++ BE/spotipity.py
@@ -4,18 +4,16 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 
-cid ="b962e9a2dfd84b49b1fb8be051f51580" 
+cid = "b962e9a2dfd84b49b1fb8be051f51580"
 secret = "abc4a22b37584663b76de6f37bb5dfcd"
 
-os.environ['SPOTIPY_CLIENT_ID']= cid
-os.environ['SPOTIPY_CLIENT_SECRET']= secret
-os.environ['SPOTIPY_REDIRECT_URI']='http://localhost:8888/callback'
+os.environ['SPOTIPY_CLIENT_ID'] = cid
+os.environ['SPOTIPY_CLIENT_SECRET'] = secret
+os.environ['SPOTIPY_REDIRECT_URI'] = 'http://localhost:8888/callback'
 username = ""
 
-
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
 
 scope = 'user-top-read playlist-modify-public'
 
@@ -31,16 +29,23 @@ token = util.prompt_for_user_token(username, scope, client_id=cid, client_secret
 
 if token:
     sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_top_tracks(limit=5, offset=0, time_range='long_term')
-    
-    top_tracks_list = [] 
-    
-    for song in results['items']: 
-        top_tracks_list.append(song) 
-    
+    results = sp.current_user_top_tracks(limit=5, offset=0, time_range='long_term') #long/medium/short ayarı buradan yapılıyor.
+
+    top_tracks_list = []
+
+    for song in results['items']:
+        album_info = {
+            #JSON outputları için filtreleme
+            "name": song['album']['name'],
+            "id": song['album']['id'],
+            "year": song['album']['release_date'][:4],
+            "link":song['album']['external_urls']
+        }
+        top_tracks_list.append(album_info)
+
     # Yeni json dosyası oluşturmak için:
     with open('top50_data.json', 'w', encoding='utf-8') as f:
-        json.dump(top_tracks_list, f, ensure_ascii=False, indent=4)  
+        json.dump(top_tracks_list, f, ensure_ascii=False, indent=4)
 
 else:
     print("Can't get token for", username)
