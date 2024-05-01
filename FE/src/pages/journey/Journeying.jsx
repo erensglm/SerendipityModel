d5179483c7a397c2df4c0e863b5466b8f602aba0
Mustafa Ensar Özdemir <94220649+m-ensr@users.noreply.github.com>
2024-05-01 14:12:02 +0300

    Update Journeying.jsx

--- FE/src/pages/journey/Journeying.jsx
+++ FE/src/pages/journey/Journeying.jsx
@@ -10,14 +10,8 @@ function Journeying() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const responses = await Promise.all(
-          Array.from({ length: 5 }, (_, index) =>
-            axios.get(`http://localhost:3030/${index}`)
-          )
-        );
-
-        const responseData = responses.map(response => response.data);
-        setData(responseData);
+        const response = await axios.get('http://localhost:5000/get_all_songs');
+        setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -28,19 +22,29 @@ function Journeying() {
 
   return (
     <div>
-        <div className="result-title"> the result of your journey</div>
+      <div className="result-title">The result of your journey</div>
       <Link className="left-arrow" to="/">
         <FontAwesomeIcon icon={faLongArrowAltLeft} />
       </Link>
       <ol>
         {data.map((item, index) => (
           <li key={index}>
-            <a className="arrow" href={`http://localhost:3030/${index}`}>
+            <a className="arrow" href={`http://localhost:5000/get_songs/${index}`}>
               <FontAwesomeIcon icon={faLongArrowAltRight} />
-              {item.title}
+              
+              {item.name} ({item.year})
             </a>
           </li>
+          
         ))}
+      </ol>
+      <ol>
+        <li>
+          <br />
+        <a className="arrow" href={`/`}> Click to create playlist with more.
+              <FontAwesomeIcon icon={faLongArrowAltRight} />
+            </a>
+        </li>
       </ol>
     </div>
   );
