6cee0685f77aadda1c119c810cfe8f0c216ccb5a
Mustafa Ensar Özdemir <94220649+m-ensr@users.noreply.github.com>
2024-05-02 16:08:22 +0300

    Update Journeying.jsx

--- FE/src/pages/journey/Journeying.jsx
+++ FE/src/pages/journey/Journeying.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowAltLeft, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
@@ -8,13 +9,25 @@ function Journeying() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDOtvven2KQD9e1pb9mgQUU1qtZsj9TwHM",
+      authDomain: "serendipity-0.firebaseapp.com",
+      databaseURL: "https://serendipity-0-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "serendipity-0",
+      storageBucket: "serendipity-0.appspot.com",
+      messagingSenderId: "308753367961",
+      appId: "1:308753367961:web:1c668fbaae58963f1c0908",
+      measurementId: "G-7L3GY2TL5K"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
     const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:5000/get_all_songs');
-        setData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+      const songsCollectionRef = collection(db, 'songs');
+      const snapshot = await getDocs(songsCollectionRef);
+      const songsData = snapshot.docs.map(doc => doc.data());
+      setData(songsData);
     };
 
     fetchData();
@@ -29,21 +42,20 @@ function Journeying() {
       <ol>
         {data.map((item, index) => (
           <li key={index}>
-            <a className="arrow" href={`http://localhost:5000/get_songs/${index}`}>
+            <a className="arrow" href={item.url} target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faLongArrowAltRight} />
-              
-              {item.name} ({item.year})
+              {item.artists.join(', ')} - {item.name} ({item.year})
             </a>
           </li>
-          
         ))}
       </ol>
       <ol>
         <li>
           <br />
-        <a className="arrow" href={`/`}> Click to create playlist with more.
-              <FontAwesomeIcon icon={faLongArrowAltRight} />
-            </a>
+          <Link className="arrow" to={`/`}>
+            Click to create playlist with more.
+            <FontAwesomeIcon icon={faLongArrowAltRight} />
+          </Link>
         </li>
       </ol>
     </div>
