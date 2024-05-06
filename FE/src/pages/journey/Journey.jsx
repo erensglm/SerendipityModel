30c03ed7b6cdd63810f76063c0c298d7602fa823
Mustafa Ensar Özdemir <94220649+m-ensr@users.noreply.github.com>
2024-05-06 16:29:41 +0300

    Update Journey.jsx

--- FE/src/pages/journey/Journey.jsx
+++ FE/src/pages/journey/Journey.jsx
@@ -1,19 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLongArrowAltLeft,
   faLongArrowAltRight,
 } from "@fortawesome/free-solid-svg-icons";
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { db } from "./firebase";
 
 function Journey() {
   const [isLoading, setIsLoading] = useState(false);
+  const [retryCount, setRetryCount] = useState(1);
 
-  const handleClick = () => {
+  useEffect(() => {
+    const intervalId = setInterval(async () => {
+      console.log("Deneme no:", retryCount);
+      const recommendedRef = collection(db, 'recommended');
+      const recommendedSnapshot = await getDocs(recommendedRef);
+      if (!recommendedSnapshot.empty) {
+        clearInterval(intervalId); // Stop the interval if data found
+        window.location.href = "/journeying";
+      } else {
+        console.log("No recommended data found.");
+        setRetryCount(prevRetryCount => prevRetryCount + 1); // Increment the retry count
+      }
+    }, 7000); // 7 seconds interval
+
+    return () => clearInterval(intervalId); // Cleanup function to clear interval on unmount
+  }, [retryCount]); // Dependency array includes retryCount to re-run effect when it changes
+
+  const handleClick = async () => {
     setIsLoading(true);
-    setTimeout(() => {
-      window.location.href = "/journeying";
-    }, 3000);
+    window.open("http://127.0.0.1:5000", "_blank");
   };
 
   return (
@@ -43,7 +61,7 @@ function Journey() {
 const LoadingDots = () => {
   const [dots, setDots] = useState("");
 
-  React.useEffect(() => {
+  useEffect(() => {
     const intervalId = setInterval(() => {
       setDots((prevDots) => (prevDots === "..." ? "" : prevDots + "."));
     }, 500);
