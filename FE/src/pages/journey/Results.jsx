aed12880283ed7c7458961eda35152cad7fb1d9e
Mustafa Ensar Özdemir <94220649+m-ensr@users.noreply.github.com>
2024-05-12 13:31:26 +0300

    Create Results.jsx

---
+++ FE/src/pages/journey/Results.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLongArrowAltLeft, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
+import { db } from "./firebase";
+
+function Results() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const songsCollectionRef = collection(db, 'recommended');
+      const snapshot = await getDocs(songsCollectionRef);
+      const songsData = snapshot.docs.slice(0, 5).map(doc => doc.data());
+      setData(songsData);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <div className="result-title">The result of your journey</div>
+      <Link className="left-arrow" to="/">
+        <FontAwesomeIcon icon={faLongArrowAltLeft} />
+      </Link>
+      <ol>
+        {data.map((item, index) => (
+          <li key={index}>
+            <a className="arrow" href={item.url} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLongArrowAltRight} />
+              {item.name} ({item.year})
+            </a>
+          </li>
+        ))}
+      </ol>
+      <ol>
+        <li>
+          <br />
+          <Link className="arrow" to={`http://localhost:5000/create_playlist`}>
+            Click to create playlist with more.
+            <FontAwesomeIcon icon={faLongArrowAltRight} />
+          </Link>
+          <br/>
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+export default Results;
