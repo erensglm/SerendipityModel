0b25659410ba21053418045ab1a48c69e049d22a
Mustafa Ensar Özdemir <94220649+m-ensr@users.noreply.github.com>
2024-05-30 22:36:47 +0300

    Update Serendipity.jsx

--- FE/src/pages/serendipity/Serendipity.jsx
+++ FE/src/pages/serendipity/Serendipity.jsx
@@ -11,7 +11,10 @@ function Serendipity() {
     <Link className="left-arrow" to="/"> 
       <FontAwesomeIcon icon={faLongArrowAltLeft} />
     </Link>
-    <div className='serendipity_cssontent'> some info about project
+    <div className='serendipity_cssontent'> Serendipity is an AI model interface trained with a lot of data (we're talking about 1,000,000 songs).
+As soon as you give permission, your most listened to songs are taken from Spotify and presented to this model.
+The model, which we assume is sufficiently trained, takes these songs as parameters and finds song suggestions that we hope it has not heard before.
+<br/>If you're a good enough listener, you can even create a playlist of these songs.
     </div>
   </div>
   )
